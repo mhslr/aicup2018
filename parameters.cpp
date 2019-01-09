59e412ca24be66f146ff1587be39d5590364453a
drhasler <45033724+drhasler@users.noreply.github.com>
2019-01-09 01:31:06 +0100

    4 CHAR names all caps

--- parameters.cpp
+++ parameters.cpp
@@ -13,11 +13,11 @@ MROB =             2.0,
 MBAL =             1.0,
 AWID =            30.0,
 ADEP =            40.0,
-TD   =          1.0/60, // tick duration
+TDUR =          1.0/60, // tick duration
 GRAV =            30.0, // gravity
-MGS  =            30.0, // max ground speed
+MAGS =            30.0, // max ground speed
 RRAD =            1.00, // robot radius
 BIGR =            1.05, // robot max radius
 BRAD =            2.00, // ball radius
-ACC  =           100.0; // max acceleration
+MACC =           100.0; // max acceleration
 
